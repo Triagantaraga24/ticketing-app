3ecb03891b57620525ccb9897198c7b55f2525f6
Raga Triagantara <90598532+Triagantaraga24@users.noreply.github.com>
2025-10-31 05:42:35 +0700

    Remove verification comment in OrderStatus display implementation
    Removed commented instruction for verification in the OrderStatus implementation.

--- src/models/order.rs
+++ src/models/order.rs
@@ -24,7 +24,6 @@ pub enum OrderStatus {
     Failed,
 }
 
-// TAMBAHKAN/VERIFIKASI BLOK INI
 impl std::fmt::Display for OrderStatus {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
